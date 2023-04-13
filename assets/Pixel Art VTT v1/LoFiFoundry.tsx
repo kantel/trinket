14d5aed27d6fa7dd097c42bd502b2f2fe083391e
Jörg Kantel <joerg@kantel.de>
2023-04-13 17:55:35 +0200

    Neue Assets

---
+++ assets/Pixel Art VTT v1/LoFiFoundry.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="LoFiFoundry" tilewidth="64" tileheight="64" tilecount="275" columns="25">
+ <image source="LoFiFoundry.png" width="1600" height="704"/>
+ <wangsets>
+  <wangset name="Walls" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="86" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="87" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="88" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="89" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="111" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="112" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="113" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="114" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="136" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="137" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="138" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="139" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="161" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="162" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="163" wangid="0,0,0,0,0,0,1,0"/>
+  </wangset>
+  <wangset name="Pavement" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="25" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="26" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="27" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="50" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="51" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="52" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="75" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="76" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="100" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="101" wangid="1,1,1,1,1,1,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
